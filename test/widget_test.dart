dd84c2dd020de37ac8a2cb11e2382f22ed5a39fa
Atik-Hridoy <atikahmedridoy@gmail.com>
2025-09-03 11:24:11 +0600

    new app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:elektro_ecommerce/main.dart';
+import 'package:elecktro_ecommerce/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
